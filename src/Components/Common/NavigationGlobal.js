885f023b5e47e8b24df37f8897b4b794fa19a02d
unknown <kwakms821@daum.net>
2020-11-14 16:19:17 +0900

    header component나누기

---
+++ src/Components/Common/NavigationGlobal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function NavigationGlobal(){
+    return (
+      <>
+        <div class="navigation navigation__global">
+          <ul class="navigation__list">
+            <li>
+              <a href="javascript:void(0)">숙소</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)">체험</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)">온라인체험</a>
+            </li>
+          </ul>
+        </div>
+      </>
+    );
+}
+
+export default NavigationGlobal;
